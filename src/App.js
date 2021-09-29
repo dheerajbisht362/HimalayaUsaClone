7b597b0cf14b297a50ba8b7ba985d3aedabbd54d
anurag99x <anuragxpandey@gmail.com>
2021-09-29 12:00:07 +0530

    signup

--- src/App.js
+++ src/App.js
@@ -3,17 +3,18 @@ import Footer from "./Components/Footer";
 // import Product from "./Components/ProductView";
 import Login from "./Components/LogIn";
 import PaymentPage from "./Components/PaymentPage";
+import { Signup } from "./Components/Signup";
 
 function App() {
-	return (
-		<div style={{ position: "relative", minHeight: "100vh" }}>
-			{/* <Product /> */}
-			<Login />
-			<PaymentPage />
-
-			<Footer />
-		</div>
-	);
+  return (
+    <div style={{ position: "relative", minHeight: "100vh" }}>
+      {/* <Product /> */}
+      {/* <Login /> */}
+      {/* <PaymentPage /> */}
+      <Signup />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
