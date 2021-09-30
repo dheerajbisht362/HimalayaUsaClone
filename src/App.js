5ae22143f30b62f24240be451dc0b81078ef1c78
kkm980 <kantkrishna751@gmail.com>
2021-09-30 19:52:09 +0530

    landing page

--- src/App.js
+++ src/App.js
@@ -6,11 +6,27 @@ import Login from "./Components/LogIn";
 import PaymentPage from "./Components/PaymentPage";
 import { Signup } from "./Components/Signup";
 import Supplements from "./Pages/SupplementsCategory";
-
+import {Textplant} from "./Components/Textplant"
+import { TextCards } from "./Components/TextCards";
+import { TextCardPrent } from "./Components/TextCardPrent";
+import {InstaReels} from "./Components/InstaReels"
+import {HeroComp} from "./Components/HeroComp"
+import {BestSeller} from "./Components/BestSeller"
+import { Shopbycatg } from "./Components/ShopbyCatg";
 function App() {
 	return (
 		<div style={{ position: "relative", minHeight: "100vh" }}>
 			<Navbar />
+            <HeroComp/>
+			<Shopbycatg/>
+
+				
+			
+			
+			<Textplant/>
+			<TextCardPrent/>
+			<InstaReels/>
+			<BestSeller/>
 			{/* <ProductPage /> */}
 			{/* <Login /> */}
 			<PaymentPage />
