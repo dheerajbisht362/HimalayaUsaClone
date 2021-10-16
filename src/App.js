b870d36171b29968d4cd7a583030ffddcdc6a650
dheerajbisht362 <dheerajbisht362@gmail.com>
2021-10-16 20:43:48 +0530

    repo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import "./App.css";
+import "./App.css"; //CSss
 import Supplements from "./Pages/SupplementsCategory";
 import LogInPage from "./Pages/LoginPage";
 import SignUpPage from "./Pages/SignUpPage";
